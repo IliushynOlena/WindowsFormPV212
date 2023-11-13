19cdcd18951cf84f9cc9de5c2667c159408b3227
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-11-13 20:09:53 +0200

    List Box, Collections

---
+++ 06_ComboBoxProject/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace _06_ComboBoxProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.orderComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.showBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.removeBtn = new System.Windows.Forms.Button();
+            this.showSelectedBtn = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select your city :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(847, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(234, 41);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select your city :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1440, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(234, 41);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Select your city :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Rivne",
+            "Odesa",
+            "Kyiv",
+            "Lviv",
+            "Poltava",
+            "Kharkiv",
+            "Uzgorod"});
+            this.comboBox1.Location = new System.Drawing.Point(150, 177);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(491, 245);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Rivne",
+            "Odesa",
+            "Kyiv",
+            "Lviv",
+            "Poltava",
+            "Kharkiv",
+            "Uzgorod"});
+            this.comboBox2.Location = new System.Drawing.Point(728, 177);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(491, 49);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Rivne",
+            "Odesa",
+            "Kyiv",
+            "Lviv",
+            "Poltava",
+            "Kharkiv",
+            "Uzgorod"});
+            this.comboBox3.Location = new System.Drawing.Point(1395, 177);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(491, 49);
+            this.comboBox3.TabIndex = 5;
+            // 
+            // orderComboBox
+            // 
+            this.orderComboBox.FormattingEnabled = true;
+            this.orderComboBox.Location = new System.Drawing.Point(150, 619);
+            this.orderComboBox.Name = "orderComboBox";
+            this.orderComboBox.Size = new System.Drawing.Size(491, 49);
+            this.orderComboBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 550);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 41);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Select order :";
+            // 
+            // showBtn
+            // 
+            this.showBtn.Location = new System.Drawing.Point(245, 439);
+            this.showBtn.Name = "showBtn";
+            this.showBtn.Size = new System.Drawing.Size(217, 78);
+            this.showBtn.TabIndex = 8;
+            this.showBtn.Text = "Show";
+            this.showBtn.UseVisualStyleBackColor = true;
+            this.showBtn.Click += new System.EventHandler(this.showBtn_Click);
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(1440, 701);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(359, 78);
+            this.addBtn.TabIndex = 9;
+            this.addBtn.Text = "Add New";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // removeBtn
+            // 
+            this.removeBtn.Location = new System.Drawing.Point(1440, 841);
+            this.removeBtn.Name = "removeBtn";
+            this.removeBtn.Size = new System.Drawing.Size(359, 78);
+            this.removeBtn.TabIndex = 10;
+            this.removeBtn.Text = "Remove Select";
+            this.removeBtn.UseVisualStyleBackColor = true;
+            this.removeBtn.Click += new System.EventHandler(this.removeBtn_Click);
+            // 
+            // showSelectedBtn
+            // 
+            this.showSelectedBtn.Location = new System.Drawing.Point(1440, 973);
+            this.showSelectedBtn.Name = "showSelectedBtn";
+            this.showSelectedBtn.Size = new System.Drawing.Size(359, 78);
+            this.showSelectedBtn.TabIndex = 11;
+            this.showSelectedBtn.Text = "Show Select";
+            this.showSelectedBtn.UseVisualStyleBackColor = true;
+            this.showSelectedBtn.Click += new System.EventHandler(this.showSelectedBtn_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(919, 605);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(222, 47);
+            this.numericUpDown1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(929, 531);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(254, 41);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Enter order price :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2188, 1135);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.showSelectedBtn);
+            this.Controls.Add(this.removeBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.showBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.orderComboBox);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private ComboBox orderComboBox;
+        private Label label4;
+        private Button showBtn;
+        private Button addBtn;
+        private Button removeBtn;
+        private Button showSelectedBtn;
+        private NumericUpDown numericUpDown1;
+        private Label label5;
+    }
+}
---
+++ 07_ListBox/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace _07_ListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.moveRightBtn = new System.Windows.Forms.Button();
+            this.moveLeftBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.showSelectionBtn = new System.Windows.Forms.Button();
+            this.removeSelectedBtn = new System.Windows.Forms.Button();
+            this.deleteCheckedBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(333, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 81);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "List Box";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(1202, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(537, 81);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Checked List Box";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 41;
+            this.listBox1.Items.AddRange(new object[] {
+            "Red",
+            "Green",
+            "Blue",
+            "Yellow",
+            "Gray",
+            "Orange",
+            "Brown"});
+            this.listBox1.Location = new System.Drawing.Point(144, 146);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(766, 660);
+            this.listBox1.TabIndex = 2;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Red",
+            "Green",
+            "Blue",
+            "Yellow",
+            "Gray",
+            "Orange",
+            "Brown"});
+            this.checkedListBox1.Location = new System.Drawing.Point(1176, 155);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(806, 664);
+            this.checkedListBox1.TabIndex = 3;
+            // 
+            // moveRightBtn
+            // 
+            this.moveRightBtn.Font = new System.Drawing.Font("Segoe UI Black", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.moveRightBtn.Location = new System.Drawing.Point(956, 280);
+            this.moveRightBtn.Name = "moveRightBtn";
+            this.moveRightBtn.Size = new System.Drawing.Size(181, 119);
+            this.moveRightBtn.TabIndex = 4;
+            this.moveRightBtn.Text = "-->";
+            this.moveRightBtn.UseVisualStyleBackColor = true;
+            this.moveRightBtn.Click += new System.EventHandler(this.moveRightBtn_Click);
+            // 
+            // moveLeftBtn
+            // 
+            this.moveLeftBtn.Font = new System.Drawing.Font("Segoe UI Black", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.moveLeftBtn.Location = new System.Drawing.Point(956, 484);
+            this.moveLeftBtn.Name = "moveLeftBtn";
+            this.moveLeftBtn.Size = new System.Drawing.Size(181, 119);
+            this.moveLeftBtn.TabIndex = 5;
+            this.moveLeftBtn.Text = "<--";
+            this.moveLeftBtn.UseVisualStyleBackColor = true;
+            this.moveLeftBtn.Click += new System.EventHandler(this.moveLeftBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(152, 871);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(597, 47);
+            this.textBox1.TabIndex = 6;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Font = new System.Drawing.Font("Segoe UI Black", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.addBtn.Location = new System.Drawing.Point(807, 835);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(103, 83);
+            this.addBtn.TabIndex = 7;
+            this.addBtn.Text = "+";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // showSelectionBtn
+            // 
+            this.showSelectionBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.showSelectionBtn.Location = new System.Drawing.Point(152, 1026);
+            this.showSelectionBtn.Name = "showSelectionBtn";
+            this.showSelectionBtn.Size = new System.Drawing.Size(393, 130);
+            this.showSelectionBtn.TabIndex = 8;
+            this.showSelectionBtn.Text = "Show Selection";
+            this.showSelectionBtn.UseVisualStyleBackColor = true;
+            this.showSelectionBtn.Click += new System.EventHandler(this.showSelectionBtn_Click);
+            // 
+            // removeSelectedBtn
+            // 
+            this.removeSelectedBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.removeSelectedBtn.Location = new System.Drawing.Point(580, 1026);
+            this.removeSelectedBtn.Name = "removeSelectedBtn";
+            this.removeSelectedBtn.Size = new System.Drawing.Size(393, 130);
+            this.removeSelectedBtn.TabIndex = 9;
+            this.removeSelectedBtn.Text = "Remove Selected Items";
+            this.removeSelectedBtn.UseVisualStyleBackColor = true;
+            this.removeSelectedBtn.Click += new System.EventHandler(this.removeSelectedBtn_Click);
+            // 
+            // deleteCheckedBtn
+            // 
+            this.deleteCheckedBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.deleteCheckedBtn.Location = new System.Drawing.Point(1490, 1026);
+            this.deleteCheckedBtn.Name = "deleteCheckedBtn";
+            this.deleteCheckedBtn.Size = new System.Drawing.Size(393, 130);
+            this.deleteCheckedBtn.TabIndex = 10;
+            this.deleteCheckedBtn.Text = "Delete Checked Items";
+            this.deleteCheckedBtn.UseVisualStyleBackColor = true;
+            this.deleteCheckedBtn.Click += new System.EventHandler(this.deleteCheckedBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2180, 1192);
+            this.Controls.Add(this.deleteCheckedBtn);
+            this.Controls.Add(this.removeSelectedBtn);
+            this.Controls.Add(this.showSelectionBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.moveLeftBtn);
+            this.Controls.Add(this.moveRightBtn);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ListBox listBox1;
+        private CheckedListBox checkedListBox1;
+        private Button moveRightBtn;
+        private Button moveLeftBtn;
+        private TextBox textBox1;
+        private Button addBtn;
+        private Button showSelectionBtn;
+        private Button removeSelectedBtn;
+        private Button deleteCheckedBtn;
+    }
+}
